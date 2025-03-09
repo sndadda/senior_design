3956c29837b2fc7a758bdd1104dd93692191b4dd
Yang Song <ys678@drexel.edu>
2025-03-09 10:35:17 -0400

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import StudentDashboard from "./pages/StudentDashboard";
 import ProfessorDashboard from "./pages/ProfessorDashboard";
+import VerifyEmail from "./pages/VerifyEmail";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -36,9 +37,9 @@ function App() {
       <Routes>
         <Route path="/" element={user ? <Navigate to={`/${user.role}_dashboard`} /> : <Login setUser={setUser} />} />
         <Route path="/signup" element={user ? <Navigate to={`/${user.role}_dashboard`} /> : <Signup />} />
-		<Route path="/verify-email" element={<VerifyEmail />} />
         <Route path="/student_dashboard" element={user?.role === "student" ? <StudentDashboard setUser={setUser} /> : <Navigate to="/" />} />
         <Route path="/professor_dashboard" element={user?.role === "professor" ? <ProfessorDashboard setUser={setUser} /> : <Navigate to="/" />} />
+		<Route path="/verify-email" element={<VerifyEmail />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
